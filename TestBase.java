db237827f017c38928a33f97c82bdb31c44b51d9
Emre BALCI <116108168+e1982b@users.noreply.github.com>
2023-06-01 19:59:06 +0200

    TestBase

---
+++ TestBase.java
@@ -0,0 +1,87 @@
+package com.amazon.test;
+
+import com.amazon.utilities.BrowserUtils;
+import com.amazon.utilities.ConfigurationReader;
+import com.amazon.utilities.Driver;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    protected WebDriver driver;
+    protected Actions actions;
+    protected WebDriverWait wait;
+    protected ExtentReports report;
+    protected ExtentHtmlReporter htmlReporter;
+    protected ExtentTest extentLogger;
+
+    @BeforeTest
+    public void setUpTest(){
+        //initialize the class
+        report=new ExtentReports();
+        //project path
+        String projectPath=System.getProperty("user.dir");
+        //String date = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        //path for report
+        String path= projectPath + "/test-output/report.html";
+        // String path= projectPath + "/test-output/report"+date+".html";
+
+        //initialize the html report with the report path
+        htmlReporter=new ExtentHtmlReporter(path);
+        //attach the html report to the report object
+        report.attachReporter(htmlReporter);
+
+        // title in report
+        htmlReporter.config().setReportName("Amazon Smoke Test");
+        report.setSystemInfo("Environment","Selling");
+        report.setSystemInfo("Browser", ConfigurationReader.get("browser"));
+        report.setSystemInfo("OS",System.getProperty("os.name"));
+        report.setSystemInfo("Test Engineer","Emre BALCI");
+
+    }
+    @AfterTest
+    public void tearDownTest(){
+        report.flush();
+    }
+
+    @BeforeMethod
+    public void setUp(){
+        driver= Driver.get();
+//        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+         driver.manage().window().maximize();
+        actions=new Actions(driver);
+        wait=new WebDriverWait(driver,15);
+        driver.get(ConfigurationReader.get("url"));
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) throws InterruptedException, IOException {
+        // if test fails
+        if(result.getStatus()==ITestResult.FAILURE){
+            //Record the name of the failed test
+            extentLogger.fail(result.getName());
+            //Take the screenshot and return the location of screenshot
+            String screenShotPath= BrowserUtils.getScreenshot(result.getName());
+            //Add the screenshot to the report
+            extentLogger.addScreenCaptureFromPath(screenShotPath);
+            //capture the exception and put inside the report
+            extentLogger.fail(result.getThrowable());
+        }
+        Thread.sleep(2000);
+        //driver.close();
+        Driver.closeDriver();
+    }
+}
